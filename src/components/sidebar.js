e52586ee2d18241b9e1631652d5147ee87ac0a40
Joey Schroeder <schrjos@gmail.com>
2018-06-07 22:57:57 -0500

    feat: Add additional band links

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -7,10 +7,12 @@ export class Sidebar extends Component {
         return (
             <div className='sidebar'>
                 <nav>
-                    <a href='./static/schroeder-joseph-web-developer-resume.pdf' target='_blank'><i className='fa fa-file-pdf-o' aria-hidden='true' /> Résumé</a>
-                    <a href='https://www.linkedin.com/in/joeyschroeder' target='_blank'><i className='fa fa-linkedin-square' aria-hidden='true' /> LinkedIn</a>
-                    <a href='https://github.com/joeyschroeder' target='_blank'><i className='fa fa-github' aria-hidden='true' /> GitHub</a>
-                    <a href='https://joeyschroeder.bandcamp.com' target='_blank'><i className='fa fa-bandcamp' aria-hidden='true' /> Bandcamp</a>
+                    <a href='./static/schroeder-joseph-web-developer-resume.pdf' target='_blank'><i className='fa fa-file-pdf-o' aria-hidden='true' /><span>Résumé</span></a>
+                    <a href='https://www.linkedin.com/in/joeyschroeder' target='_blank'><i className='fa fa-linkedin-square' aria-hidden='true' /><span>LinkedIn</span></a>
+                    <a href='https://github.com/joeyschroeder' target='_blank'><i className='fa fa-github' aria-hidden='true' /><span>GitHub</span></a>
+                    <a href='https://joeyschroeder.bandcamp.com' target='_blank'><i className='fa fa-bandcamp' aria-hidden='true' /><span>Bandcamp</span></a>
+                    <a href='https://theapacherevolver.bandcamp.com/' target='_blank'><i className='fa fa-bandcamp' aria-hidden='true' /><span>The Apache Revolver</span></a>
+                    <a href='https://computerjones.bandcamp.com/' target='_blank'><i className='fa fa-bandcamp' aria-hidden='true' /><span>Computer Jones</span></a>
                     {/* <button><i class='fa fa-envelope' aria-hidden='true' /> Contact</button> */}
                 </nav>
             </div>
