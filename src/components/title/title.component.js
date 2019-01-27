d6408701a844610e7a3c951ba2423dad57a71b83
Joey Schroeder <schrjos@gmail.com>
2019-01-27 16:54:55 -0600

    feat: Add open graph image

--- src/components/title/title.component.js
+++ src/components/title/title.component.js
@@ -3,7 +3,7 @@ import styles from './title.style.scss';
 
 export const Title = () => (
   <h2 className={styles.root}>
-    <span className={styles.content}>Designer//</span>
-    <span className={styles.content}>Developer</span>
+    <span className={styles.content}>Developer//</span>
+    <span className={styles.content}>Designer</span>
   </h2>
 );
