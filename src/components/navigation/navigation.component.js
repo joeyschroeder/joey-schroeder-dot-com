669553e6b65733243bf4a704017e031c3de133d5
Joey Schroeder <schrjos@gmail.com>
2020-05-28 22:44:28 -0500

    refactor: Move navigation items to constant

--- src/components/navigation/navigation.component.js
+++ src/components/navigation/navigation.component.js
@@ -2,58 +2,27 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { NavigationItem } from '../navigation-item/navigation-item.component';
 import styles from './navigation.style.scss';
-import resumePdf from '../../assets/pdfs/schroeder-joey-web-developer-designer-resume.pdf';
+import { NAVIGATION_ITEMS } from '../../constants/navigation-items';
 
 export const Navigation = (props) => {
   const { active } = props;
 
-  return (
-    <div className={styles.root}>
+  const navigationItems = NAVIGATION_ITEMS.map((navigatonItem) => {
+    const { download, href, iconName, title } = navigatonItem;
+
+    return (
       <NavigationItem
         active={active}
-        download="schroeder-joey-developer-designer-resume.pdf"
-        href={resumePdf}
-        iconName="fas fa-file-pdf"
-        title="Résumé"
+        download={download}
+        href={href}
+        iconName={iconName}
+        key={title}
+        title={title}
       />
-      <NavigationItem
-        active={active}
-        href="https://www.linkedin.com/in/joeyschroeder"
-        iconName="fab fa-linkedin"
-        title="LinkedIn"
-      />
-      <NavigationItem
-        active={active}
-        href="https://github.com/joeyschroeder"
-        iconName="fab fa-github"
-        title="GitHub"
-      />
-      <NavigationItem
-        active={active}
-        href="https://www.npmjs.com/~joeyschroeder"
-        iconName="fab fa-npm"
-        title="NPM"
-      />
-      <NavigationItem
-        active={active}
-        href="https://joeyschroeder.bandcamp.com"
-        iconName="fab fa-bandcamp"
-        title="Bandcamp"
-      />
-      <NavigationItem
-        active={active}
-        href="https://theapacherevolver.bandcamp.com"
-        iconName="fab fa-bandcamp"
-        title="The Apache Revolver"
-      />
-      <NavigationItem
-        active={active}
-        href="https://computerjones.bandcamp.com"
-        iconName="fab fa-bandcamp"
-        title="Computer Jones"
-      />
-    </div>
-  );
+    );
+  });
+
+  return <div className={styles.root}>{navigationItems}</div>;
 };
 
 Navigation.propTypes = {
---
+++ src/constants/navigation-items.js
@@ -0,0 +1,40 @@
+import resumePdf from '../assets/pdfs/schroeder-joey-web-developer-designer-resume.pdf';
+
+export const NAVIGATION_ITEMS = [
+  {
+    download: 'schroeder-joey-developer-designer-resume.pdf',
+    href: resumePdf,
+    iconName: 'fas fa-file-pdf',
+    title: 'Résumé',
+  },
+  {
+    href: 'https://www.linkedin.com/in/joeyschroeder',
+    iconName: 'fab fa-linkedin',
+    title: 'LinkedIn',
+  },
+  {
+    href: 'https://github.com/joeyschroeder',
+    iconName: 'fab fa-github',
+    title: 'GitHub',
+  },
+  {
+    href: 'https://www.npmjs.com/~joeyschroeder',
+    iconName: 'fab fa-npm',
+    title: 'NPM',
+  },
+  {
+    href: 'https://joeyschroeder.bandcamp.com',
+    iconName: 'fab fa-bandcamp',
+    title: 'Bandcamp',
+  },
+  {
+    href: 'https://theapacherevolver.bandcamp.com',
+    iconName: 'fab fa-bandcamp',
+    title: 'The Apache Revolver',
+  },
+  {
+    href: 'https://computerjones.bandcamp.com',
+    iconName: 'fab fa-bandcamp',
+    title: 'Computer Jones',
+  },
+];
