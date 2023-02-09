2f902555e6564d85fd307feaa2a02722c103491d
Joey Schroeder <schrjos@gmail.com>
2023-02-09 09:54:37 -0600

    chore: Update react

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
-import { AppContainer } from 'react-hot-loader';
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { App } from './components/app/app';
 
-const init = (Component) =>
-  render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    document.getElementById('app')
-  );
+const root = createRoot(document.getElementById('app'));
+const init = (Component) => root.render(<Component />);
 
 init(App);
-
-if (module.hot) {
-  module.hot.accept('./components/app/app', () => {
-    // eslint-disable-next-line global-require
-    const nextApp = require('./components/app/app').App;
-    init(nextApp);
-  });
-}
